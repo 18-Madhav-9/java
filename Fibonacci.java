815a23b3d967c1e898289706ea310941c93b6744
18-madhav-9 <mash2172003@gmail.com>
2025-06-20 16:46:53 +0530

    better readable edit  in fibbonaci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -7,25 +7,12 @@ public class Fibonacci {
         
         if ( n == 0) return 0 ;
         else if ( n == 1 ) return 1 ;
-        else {
-            int fib1 = 0 ;
-            int fib2 = 0 ;
-            if ( map.containsKey(n-1) ) {
-                fib1 = map.get(n-1) ;
-            }
-            else {
-                map.put(n-1,fib(n-1)) ;
-                fib1 = map.get(n-1) ;
-            }
-            if ( map.containsKey(n-2) ) {
-                fib2 = map.get(n-2) ;
-            }
-            else {
-                map.put(n-2, fib(n-2));
-                fib2 = map.get(n-2) ;
-            }
-            map.put(n, fib1+fib2) ;
-            return fib1+fib2 ;
+        int result = 0 ;
+        if ( map.containsKey(n) ){
+            result = map.get(n);
         }
+        result = fib(n-1)+fib(n-2) ;
+        map.put(n, result);
+        return result ;
     }
 }
